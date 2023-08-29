3c754559726e945b6b3493a3260b2eeb4f15239e
karolineterra <terra.karoline1@gmail.com>
2023-08-29 16:42:37 -0300

    convertendo o modelo da pokeapi para um modelo proprio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,16 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map(
-    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
-  );
-}
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-          <span class="number">#${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}">
+          <span class="number">#${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
           <div class="detail">
             <ol class="types">
-              ${convertPokemonTypesToLi(pokemon.types).join("")}
+              ${pokemon.types
+                .map((type) => `<li class="type">${type}</li>`)
+                .join("")}
             </ol>
             <img
-              src="${pokemon.sprites.other.dream_world.front_default}"
+              src="${pokemon.photo}"
               alt="${pokemon.name}"
             />
           </div>
@@ -23,5 +20,6 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join("");
+  const newHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML = newHtml;
 });
